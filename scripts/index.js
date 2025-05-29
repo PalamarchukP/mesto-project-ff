a02a858d41064002331d012298aa871456a443f4
Polina Palamarchuk <polina.palamarchuk14@gmail.com>
2025-05-29 20:00:15 +0200

    first-try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+function createCard(cardInfo, deleteCard){
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
+    cardElement.querySelector('.card__image').src = cardInfo.link;
+    cardElement.querySelector('.card__image').alt = cardInfo.name;
 
-// @todo: DOM узлы
+    // находим кнопку удаления
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-// @todo: Функция создания карточки
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(cardElement){
+    cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+const cardsList = document.querySelector('.places__list');
+
+initialCards.forEach((item) => {
+    const createdCard = createCard(item, deleteCard);
+    cardsList.append(createdCard);
+})
